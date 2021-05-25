2f75afba47250d266e57687d99cb5e782af91305
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-25 03:38:18 -0700

    update

---
+++ src/components/QuizData.js
@@ -0,0 +1,33 @@
+export const QuizData = [
+    {
+      id: 0,
+      question: "What is the best way to have a visual struddle eating experience?",
+      options: ['standing on top of a building','private jet!', "do it in the day", "struddle and pie at same time"],
+      answer: 'private jet!'
+    },
+    {
+      id: 1,
+      question: "how to you fix a crooked eyes?",
+      options: ['you cannot fix it','slap that face real hard with the palm of your hand', "do some hoppings", "bend over"],
+      answer: 'you cannot fix it duh'
+    },
+    {
+      id: 2,
+      question: "What is the best way to have a visual sexual experience?",
+      options: ['scissors','like a dog', "hard as rock", "!handstad"],
+      answer: '!handstad!'
+    },
+    {
+      id: 3,
+      question: "What is the best way to have a visual sexual experience?",
+      options: ['scissors','like a dog', "hard as rock", "!handstad"],
+      answer: '!handstad!'
+    },
+    {
+      id: 4,
+      question: "What is the best way to have a visual sexual experience?",
+      options: ['scissors','like a dog', "hard as rock", "!handstad"],
+      answer: '!handstad!'
+    },
+
+]
