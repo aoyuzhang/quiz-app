5e91fa961b3e38da32d819555c9a6e651d1c5cb1
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-25 04:11:10 -0700

    created question list, question display componenets

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,2 +1,94 @@
-import React, {Componenet } from 'react'
+import React, {Component } from 'react'
 import {QuizData} from './QuizData'
+
+class Quiz extends Component{
+  state = {
+    userAnswer: null,
+    currentIndex: 0,
+    options: [],
+    quizEnd: false,
+    score: 0,
+    disabled: true
+  }
+
+  loadQuiz = () => {
+    const {currentIndex} = this.state
+    this.setState(() =>{
+      return {
+          question: QuizData[currentIndex].question,
+          options: QuizData[currentIndex].options,
+          answer: QuizData[currentIndex].answer
+      }
+    })
+  }
+
+  nextQuestionHander = () =>{
+    const {userAnswer, answer, score} = this.state
+    this.setState({
+      currentIndex: this.state.currentIndex +1
+    })
+    if(userAnswer === answer){
+      this.setState({
+        score: score + 1
+      })
+    }
+  }
+
+  componentDidMount(){
+    this.loadQuiz();
+  }
+  componentDidUpdate(prevProps, prevState){
+    const{currentIndex} = this.state;
+    if(this.state.currentIndex !== prevState.currentIndex){
+      this.setState( () => {
+        return {
+          disabled: true,
+          question: QuizData[currentIndex].question,
+          options: QuizData[currentIndex].options,
+          answer: QuizData[currentIndex].answer
+        }
+      });
+    }
+  }
+
+  checkAnswer = answer => {
+    this.setState({
+      userAnswer: answer,
+      disable: false
+    })
+  }
+
+  finishHandler =() => {
+    if(this.state.currentIndex === QuizData.length -1){
+      this.setState({
+        quizEnd: true
+      })
+    }
+  }
+  render(){
+    const {question, options, currentindex, userAnswer, quizEnd} = this.state
+    if(quizEnd){
+      return(
+        <div>
+          <h1>Game Over, Final score is {this.state.score} points</h1>
+          <p>The correct Answers for the quiz are</p>
+          <ul>
+            {QuizData.map((item, index) =>(
+              <li className = 'options'
+                key ={index}>
+                  {item.answer}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    }
+    return(
+      <div>
+        <h2>{question}</h2>
+      </div>
+    )
+  }
+
+}
+export default Quiz
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Quiz from './components/Quiz'
+
+// import './style.css';
+import {QuizData} from './components/QuizData';
+
+
+// import './index.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+function App(){
+  return (
+    <div classNmae = "App">
+      <Quiz/>
+    </div>
+  )
+}
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App></App>, rootElement)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
